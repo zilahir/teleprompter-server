4cdbb1cd2b859c5a0269db7ffd1bc760a86d1289
Richard Zilahi <richard.zilahi@salesframe.com>
2020-01-27 16:10:13 +0200

    server listen

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
