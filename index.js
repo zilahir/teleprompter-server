d7c1490325c15049077943914bab631542b12df5
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-07 23:32:46 +0200

    connected login api endpoint

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.send(200);
+    } else {
+        return next();
+    }
+});
+
 app.use(bodyParser.json());
+
 AuthorizationRouter.routesConfig(app);
 UsersRouter.routesConfig(app);
 PrompterRouter.routesConfig(app);
+
 server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
 
 io.on("connection", socket => {
