285040d891547fd0e11a34cc7fb1afe8a1a36217
Richard Zilahi <richard.zilahi@salesframe.com>
2020-03-07 21:30:36 +0200

    implementing authentication

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const bodyParser = require('body-parser');
 const io = require("socket.io")(server);
+const UsersRouter = require('./users/routes.config');
+const AuthorizationRouter = require('./authorization/routes.config');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+AuthorizationRouter.routesConfig(app);
+UsersRouter.routesConfig(app);
 server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
 
 io.on("connection", socket => {
@@ -18,6 +25,8 @@ io.on("connection", socket => {
 
 app.get('/', function (req, res) {
     res.send({
-        isSuccess: true,
+        isSuccess: false,
+        mongoUrl: process.env.MONGOURL,
     })
-  })
+})
+
---
+++ users/routes.config.js
@@ -0,0 +1,27 @@
+const UsersController = require('./controllers/users.controller');
+const PermissionMiddleware = require('../common/middlewares/auth.permission.middleware');
+const ValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
+
+exports.routesConfig = function (app) {
+    app.post('/users', [
+        UsersController.insert
+    ]);
+    app.get('/users', [
+        ValidationMiddleware.validJWTNeeded,
+        UsersController.list
+    ]);
+    app.get('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersController.getById
+    ]);
+    app.patch('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersController.patchById
+    ]);
+    app.delete('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        UsersController.removeById
+    ]);
+};
