a265a8b320d4328482b9072b33a1fb80756621a5
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-10 20:48:02 +0300

    implementing email sending service

---
+++ services/email.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const environment = process.env;
+
+module.exports.SMTPTransport = nodemailer.createTransport({
+    host: environment.SMTP_SERVICE_HOST,
+    port: environment.SMTP_SERVICE_PORT,
+    secure: environment.SMTP_SERVICE_SECURE, // upgrade later with STARTTLS
+    debug: true,
+    auth: {
+        user: environment.SMTP_USER_NAME,
+        pass: environment.SMTP_USER_PASSWORD
+    }
+});
+
+module.exports.ViewOption = (transport, hbs) => {
+    transport.use('compile', hbs({
+            viewPath: 'views/email',
+            extName: '.hbs'
+    }));
+}
